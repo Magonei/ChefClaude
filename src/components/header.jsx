e03650ea6b936ceb23706a4a6adbc42c33c38818
Mario González Jiménez <80714679+Magonei@users.noreply.github.com>
2025-05-11 16:33:48 +0200

    fix image

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,7 +1,9 @@
+import logo from "src/images/chef-claude-icon.png"
+
 export default function header() {
     return (
         <header>
-            <img src="src/images/chef-claude-icon.png" className="logo" alt="logo"/>
+            <img src={logo} className="logo" alt="logo"/>
             <span className="appName">Chef Claude</span>
         </header>
     )
